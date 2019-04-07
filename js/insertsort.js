f1861e74d1c037c573d07ee21d3c0f807d8c2030
Brett <brett_matsumoto@yahoo.com>
2019-04-06 18:14:29 -1000

    one solution to insertSort

---
+++ js/insertsort.js
@@ -0,0 +1,24 @@
+function arrayGenerator() {
+  let randomArray = [];
+  for (let i = 0; i < 10; i++) {
+    let randomNumber = Math.floor(Math.random() * 100);
+    randomArray.push(randomNumber);
+  }
+  console.log('testArr:', randomArray)
+  return randomArray
+}
+
+function insertionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let temp = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > temp) {
+      arr[j+1] = arr[j];
+      j--
+    }
+    arr[j + 1] = temp;
+  }
+  return arr
+}
+
+console.log(insertionSort(arrayGenerator()));
