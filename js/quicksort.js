d12c0976d400ba3ca0007a6234a796a966d4127d
Brett <brett_matsumoto@yahoo.com>
2019-04-09 01:35:21 -1000

    reformatted code

--- js/quicksort.js
+++ js/quicksort.js
@@ -1,10 +1,11 @@
 function arrayGenerator() {
   let randomArray = [];
+
   for (let i = 0; i < 10; i++) {
     let randomNumber = Math.floor(Math.random() * 100);
     randomArray.push(randomNumber);
   }
-  // console.log('testArr:', randomArray)
+
   return randomArray
 }
 
@@ -14,21 +15,23 @@ function quickSort(arr) {
   }
 
   let pivot = arr[0];
-
   let left = [];
   let right = [];
 
   for (let i = 0; i < arr.length; i++) {
+
     if (arr[i] < pivot) {
       left.push(arr[i])
     }
+
     if (arr[i] > pivot) {
       right.push(arr[i])
     }
   }
+
   return quickSort(left).concat(pivot, quickSort(right));
 }
 
-// console.log(quickSort(arrayGenerator()));
+console.log(quickSort(arrayGenerator()));
 
 
--- js/selectionionsort.js
+++ js/selectionionsort.js
@@ -1,35 +1,39 @@
 function arrayGenerator() {
   let randomArray = [];
+
   for (let i = 0; i < 10; i++) {
     let randomNumber = Math.floor(Math.random() * 100);
     randomArray.push(randomNumber);
   }
-  console.log('testArr:', randomArray)
+
   return randomArray
 }
 
 function swap(arr, i, j){
-  let temp = arr[i]; //sets temp variable to the value of index 'i' of the array
-  arr[i] = arr[j]; //sets 
+
+  let temp = arr[i]; 
+  arr[i] = arr[j]; 
   arr[j] = temp;
 }
 
 function selectionSort(arr){
+
   for(let i = 0; i < arr.length; i++){
     let min = i;
+
     for (let j = i + 1; j < arr.length; j++){
+
       if (arr[j] < arr[min]) {
         min = j;
       }
     }
+
     if (i !== min){
       swap (arr, i, min);
     }
   }
+  
   return arr;
 }
 
 console.log(selectionSort(arrayGenerator()))
-
-
-/
